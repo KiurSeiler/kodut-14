18dde48137793b4b73f6d5b8d68f6fbcf02b7af4
KiurSeiler <seiler.kiur@hotmail.com>
2022-12-20 16:50:55 +0200

    Update kodutöö14.py
    mõned muudatused

--- kodutöö14.py
+++ kodutöö14.py
@@ -15,29 +15,36 @@ n = 0
 with open('nimed.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
+        #5for row in reader:
+            #content = list(row[i] for i in int('vanus'))
+            #print (content)
+
         nimed.append(Person(row['Eesnimi'], row['Perekonnanimi'], row ['vanus']))
-        if Person('Eesnimi').isupper and Person('Perekonnanimi').isupper:
-        
-            n = n + 1
 
-            random.shuffle(nimed)
+        n = n + 1
 
-            max_num = int(input("sisesta max num: "))
 
-            gruppide_arv = math.ceil(len(nimed) / max_num)
+random.shuffle(nimed)
 
-            gruppid = []
-            for i in range (0, gruppide_arv):
-                gruppid.append([])
+max_num = int(input("sisesta max num: "))
 
-            i = 0
-            for nimi in nimed:
-                gruppid[i % gruppide_arv].append(nimi)
-                i = i + 1
 
-            for grupp in gruppid:
-                print("======")
-                for isik in grupp:
-                    print(f"{isik.first_name},{isik.last_name},{isik.age}")
-        else:
-            break
+gruppide_arv = math.ceil(len(nimed) / max_num)
+
+gruppid = []
+for i in range (0, gruppide_arv):
+    gruppid.append([])
+
+file = open('grupp.csv', 'w')
+writer = csv.writer(file)
+i = 0
+for nimi in nimed:
+    writer.writerow(f"{nimi.first_name},{nimi.last_name},{nimi.age}")
+    gruppid[i % gruppide_arv].append(nimi)
+    i = i + 1
+
+for grupp in gruppid:
+    print("======")
+    for isik in grupp:
+        print(f"{isik.first_name},{isik.last_name},{isik.age}")
+        #writer.writerow(f"{isik.first_name},{isik.last_name},{isik.age}")
